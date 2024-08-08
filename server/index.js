1464f63f9cc42b2366f83b2ca195f043e53edfab
sejal99 <sejal8974@gmail.com>
2024-08-08 22:35:38 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
 import express from "express";
 import { Server } from "socket.io";
+import {createServer} from "http";
 import dotenv from 'dotenv'
 dotenv.config()
 const app=express();
 
 app.use(express.json());
 
-const server=new Server(app);
+const server= createServer(app);
 const io=new Server(server)
 
+app.get("/",(req,res)=>{
+    res.send('hello');
+    
+})
+
+io.on("connection",(socket)=>{
+console.log("user connected");
+console.log("id",socket.id);
+
+
+})
 const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {

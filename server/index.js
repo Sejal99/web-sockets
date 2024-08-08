ea959c6f9c0b19bc2f18f638e58d62241e906c40
sejal99 <sejal8974@gmail.com>
2024-08-08 22:29:01 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app=express();
 app.use(express.json());
 
 const server=new Server(app);
-const io=new Server(server,{})
+const io=new Server(server)
 
 const PORT = process.env.PORT || 3002;
 

7c81bf75148c8e56a6564ec7cc41ecd1149fb6ad
sejal99 <sejal8974@gmail.com>
2024-08-09 22:45:59 +0530

    socket connected

--- server/index.js
+++ server/index.js
@@ -1,42 +1,42 @@
-import express from "express";
-import { Server } from "socket.io";
-import { createServer } from "http";
-import dotenv from "dotenv";
-import cors from "cors";
-dotenv.config();
-const app = express();
+// import express from "express";
+// import { Server } from "socket.io";
+// import { createServer } from "http";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// dotenv.config();
+// const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-const server = createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
+// const server = createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   },
+// });
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   })
+// );
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+// app.get("/", (req, res) => {
+//   res.send("hello");
+// });
 
-io.on("connection", (socket) => {
-  console.log("user connected");
-  console.log("id", socket.id);
-  socket.emit("welcome","welcome to server")//sends to all the active servers
-  socket.broadcast.emit("welcome", `${socket.id} User joined the server`) //sends to all servers except this server id
-});
-const PORT = process.env.PORT || 3002;
+// io.on("connection", (socket) => {
+//   console.log("user connected");
+//   console.log("id", socket.id);
+//   socket.emit("welcome","welcome to server")//sends to all the active servers
+//   socket.broadcast.emit("welcome", `${socket.id} User joined the server`) //sends to all servers except this server id
+// });
+// const PORT = process.env.PORT || 3002;
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
